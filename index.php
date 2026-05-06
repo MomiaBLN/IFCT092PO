c869dbbe74016861e4cfa6df1543a19cc5cab580
Iván Cerezo Vivas <ivan.cerezo.vivas@gmail.com>
2026-05-06 19:29:44 +0200

    Arrays y arrays asociativos. Aprendemos a aglutinar datos en una misma variable

--- index.php
+++ index.php
@@ -2,10 +2,59 @@
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <title>Tablas salariales en tecnología</title>
+    <title>¡Hola!</title>
 </head>
 <body>
-    <h1>Descubre el salario asignado por convenio según tu categoría</h1>
-    <?php require('formulario.php'); ?>
+    <h1>
+        <?php
+        echo "¡Hola, mundo!";
+        ?>
+    </h1>
+    <p>
+        <?php
+        
+        $nombres = ["Persona1", "Persona2", "Persona3", "Persona4", "Persona5", "Persona6", "Persona7", "Persona8", "Persona9"];
+
+        echo "Hola, " . $nombres[7];
+        echo ". ¿Qué tal, " . $nombres[0] . "?";
+        
+        //for ($i = 0; $i < 6; $i++)
+        for ($i = 0; $i < count($nombres); $i++)
+        {
+            echo "$nombres[$i], ";
+        }
+
+        $productoChampu = [
+            "id" => 349234,
+            "nombre" => "Champú",
+            "precio" => 3, 
+        ];
+
+        $productoBoligrafo = [
+            "id" => 872342,
+            "nombre" => "Bolígrafo",
+            "precio" => 1,
+        ];
+
+        echo $productoChampu["id"];
+
+        $libro = [
+            "titulo" => "Aprendiendo php",
+            "isbn" => "09807237623423",
+            "autoria" => "Iván",
+            "fecha" => "22/04/2026",
+            "precio" => "9.99€"
+        ];
+
+        echo $libro["titulo"];
+        echo " (";
+        echo $libro["fecha"];
+        echo ") ";
+        echo $libro["autoria"];
+        echo ": ";
+        echo $libro["precio"];
+
+        ?>
+    </p>
 </body>
 </html>
