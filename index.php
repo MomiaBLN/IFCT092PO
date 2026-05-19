9a97a77380ecfbaae5cfc389ea15a93bf7a16f42
Iván Cerezo Vivas <ivan.cerezo.vivas@gmail.com>
2026-05-19 14:13:08 +0200

    Inicio proyecto cartera de clientes con Jaime

--- index.php
+++ index.php
@@ -2,24 +2,8 @@
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <title>Tablas salariales en tecnología</title>
+    <title>Cartera de clientes</title>
 </head>
 <body>
-    <h1>Descubre el salario asignado por convenio según tu categoría</h1>
-    <?php
-        //Modelo
-        require_once("modelo/area.php");
-        require_once("modelo/grupo.php");
-        require_once("modelo/nivel.php");
-        require_once("modelo/categoria.php");
-        require_once("modelo/datos-categorias.php");
-
-        //Controlador
-        require_once("controlador/controlador-categorias.php");
-        $controlador = new ControladorCategorias($listaDeCategorias);
-
-        //Vista
-        require_once('vista/formulario.php');
-    ?>
 </body>
 </html>
